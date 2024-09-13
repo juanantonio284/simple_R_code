4a6250d2bfd624e786714df3a3c89f9dc7b2739b
JA-Ubuntu <juanantonioteran@protonmail.com>
2024-09-13 18:56:07 -0500

    csv_reader_with_fread.R V2

--- csv_reader_with_fread.R
+++ csv_reader_with_fread.R
@@ -1,28 +1,25 @@
 # This reader uses the data.table package's fread instruction which reads faster (maybe), and
 # automatically converts to data.table format.
 
+message( paste0( rep( "-", 100 ), collapse = "" ) )
+
 library(data.table)
 
-message( paste0( rep( "-", 100 ), collapse = "" ) )
-message( "\t", "Reading" )
 data_dir <- "~/Downloads/"
 read_dir <- paste0( data_dir, "name_of_specific_folder_with_data" )
 
-# 1 CSV using fread  -------------------------------------------------------------------------------
+
+# 1 Read CSV using fread ---------------------------------------------------------------------------
+message( "\t", "\t", "Reading: ", file_name )
+
 object_name <- "name_of_file_without_extension" # .... (i)
 file_name <- paste0(object_name, ".csv") 
 full_file_path <- paste0( read_dir, file_name )
 
-message( "\t", "\t", "Reading: ", file_name )
-
 assign( 
-  x = object_name, # .... (ii)
+  x = object_name,
   value = data.table::fread(full_file_path) 
 )
-
-
-
-
 
 # (i) hopefully your file names are nice (short, clear, no weird characters) and it makes sense to
 #     use them as the names of the objects inside your R environment.
@@ -31,4 +28,27 @@ assign(
 #
 #     If the name of the file is something like "gdp_unitd ArAb EMIrates_as of 11 22-2010.csv"
 #     you may want to change it ... But, if you don't want to change it for reproducibility reasons,
-#     then change the assignment line (ii).
+#     then change wherever you see "object_name" to a name between quotation marks, e.g. "new_name"
+
+# save ---------------------------------------------------------------------------------------------
+message( "\t", "Saving" )
+message( "\t", "\t", object_name, ".rds" )
+
+write_dir <- read_dir # change if necessary
+
+# for a single object ............................
+save( INSERT_OBJECT_NAME_HERE_NO_QUOTATION_MARKS, 
+      file = paste0( write_dir,
+                     object_name,
+                     ".rds" ) 
+)
+
+# for many objects ...............................
+# save( first object, second object,
+#       file = paste0( write_dir,
+#                      "INSERT FILE NAME HERE BETWEEN QUOTATION MARKS",
+#                      ".RData" ) 
+# )
+
+message( "\t", "Finished" )
+
