32ee549b5cb315220c4828b353d20ff7ef2e19d7
JA-Ubuntu <juanantonioteran@protonmail.com>
2024-09-13 18:55:30 -0500

    table explorer and readme update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simple_R_code
 R code for everyday tasks
+
+* `csv_reader_with_fread.R`: read a CSV file into R transforming its contents to data.table format;
+  save the data.table object in a binary file with .RDS or .RDATA extension for easy loading
+
+* `table_explorer.R`: code for basic exploration of large data tables
+
--- table_explorer.R
+++ table_explorer.R
@@ -1,32 +1,52 @@
 # Code to explore data.table objects
 # (see below for links to download sample data)
 
-# 1 Basics -----------------------------------------------------------------------------------------
+library(data.table)
+
+# Basics -------------------------------------------------------------------------------------------
 
 # See the structure of the table
-cstr(tables)
+cstr(WPP_lt_1950_2023)
 # cstr is a custom function to get the "clean" structure of the table 
 # (change to str if you don't have cstr)
 
 # get a vector like c( "column_1", "column_2", ... ) with the column names
-dput( names(tables) )
+dput( names(WPP_lt_1950_2023) )
 
 # make a smaller table with only one row
-x <-tables[1, ]
+x <-WPP_lt_1950_2023[1, ]
 
 # make a smaller table with filtering conditions and only 10 rows
-x <- tables[ LocID == 32 & Time == 1950, ][1:10, ]
+x <- WPP_lt_1950_2023[ LocID == 32 & Time == 1950, ][1:10, ]
 
 
-# 2 Select columns ---------------------------------------------------------------------------------
+# Further ------------------------------------------------------------------------------------------
+
+# 1. select unique values from the location id column and the country code column
+country_codes <- unique( 
+  WPP_lt_1950_2023[ , list(LocID, ISO3_code, Location) ] 
+)
+
+# 2. keep only rows where ISO3_code column is not blank
+country_codes <- country_codes[ ISO3_code != "", ]
+# This is because only countries are needed for this example and, upon inspection, you see that many
+# codes do not correspond to a country but rather a region (e.g. Europe and Northern America).
+
+# 3. sort alphabetical
+data.table::setorder(country_codes, ISO3_code) 
+
+View(country_codes)
+
+
+# Select columns -----------------------------------------------------------------------------------
 # Select columns to make a smaller, easier to read, table
 
 # (i) put all the column names in the vector ...............
-columns_wanted <- dput( names(tables) ) 
+columns_wanted <- dput( names(WPP_lt_1950_2023) ) 
 
 # line above does the same as this
 columns_wanted <- c(
-  "SortOrder", "LocID", "Notes", "ISO3_code", "ISO2_code", "SDMX_code",  "LocTypeID", "LocTypeName",
+  "SortOrder", "LocID", "Notes", "ISO3_code", "ISO2_code", "SDMX_code", "LocTypeID", "LocTypeName",
   "ParentID", "Location", "VarID", "Variant", "Time", "MidPeriod", "SexID", "Sex", "AgeGrp", 
   "AgeGrpStart", "AgeGrpSpan", "mx", "qx", "px", "lx", "dx", "Lx", "Sx", "Tx", "ex", "ax"
 )
@@ -42,18 +62,25 @@ columns_wanted <- c(
 )
 
 # (iii) subset .............................................
-smaller_table <- tables[ , columns_wanted, with = FALSE ] # preferred method "with = FALSE"
-
-# Other methods ............................................
-# smaller_table <- tables[ , ..columns_wanted ] # '..' if using vector and NOT using "with = FALSE"
-
-# smaller_table <- tables[ , list(LocID, Location) ] # list the columns directly
-                                                     # also takes'.()' shortcut instead of 'list()'
+smaller_table <- WPP_lt_1950_2023[ , columns_wanted, with = FALSE ] # preferred method "with = FALSE"
 
 
-# Sample Data ---------------------------------------------------------------------------------
-# dataset used here
+# Other methods
+# smaller_table <- WPP_lt_1950_2023[ , ..columns_wanted ] # use '..' syntax if using vector and NOT 
+                                                          # using "with = FALSE"
+
+# smaller_table <- WPP_lt_1950_2023[ , list(LocID, Location) ] # list the columns directly
+                                                               # also takes'.()' shortcut
+                                                               # instead of 'list()'
+
+# (iv) you could also delete columns you don't want
+# WPP_lt_1950_2023[ , c( "SortOrder", "LocID", "Notes" ) := NULL ] # note vector and "" syntax
+
+
+# Sample Data ......................................................................................
+
+# direct link to download dataset 
 # https://population.un.org/wpp/Download/Files/1_Indicator%20(Standard)/CSV_FILES/WPP2024_Life_Table_Abridged_Medium_1950-2023.csv.gz
 
-# from this page
+# home page
 # https://population.un.org/wpp/Download/Standard/CSV/
