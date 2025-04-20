6a4baefbe27c8231a33291515903139dbcd19bd6
JA-Ubuntu <juanantonioteran@protonmail.com>
2025-04-19 19:08:31 -0500

    first

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # simple_R_code
 R code for everyday tasks
 
-* `csv_reader_with_fread.R`: read a CSV file into R transforming its contents to data.table format;
+* `csv_reader_with_fread`: read a CSV file into R transforming its contents to data.table format;
   save the data.table object to a binary file with .rds or .RData extension for easy loading
 
-* `table_explorer.R`: code for basic exploration of large data tables
+* `table_explorer`: code for basic exploration of large data tables
 
 * `saver`: different options to save R objects to binary files with .rds or .RData extension
 
-* `extract_tables_from_pdf_1.R`: code to extract tables from pdf files (very basic, does not fix
+* `extract_tables_from_pdf_1`: code to extract tables from pdf files (very basic, does not fix
   misreads)
 
-* `extract_tables_from_pdf_2.R`: code to extract tables from pdf files, fix misreads, and wrangle
+* `extract_tables_from_pdf_2`: code to extract tables from pdf files, fix misreads, and wrangle
   data
+  
+* `looper`: an example of using expression parsing to run commands in a loop
---
+++ looper.R
@@ -0,0 +1,50 @@
+# USING EXPRESSION PARSING TO RUN COMMANDS IN A LOOP
+
+# Much of the code below is related to a particular example which doesn't really matter; what's
+# relevant is the usage of the following functions
+# * "expression"
+# * "deparse"
+# * "gsub" (gsub is a common R function which you should learn separately but I point it out because
+#    in this script there is a nice example of its usage)
+
+# set reading parameters ---------------------------------------------------------------------------
+years <- c(1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020)
+
+country_prefix <- c( "arg", "bol", "bra", "chi", "col", "cri", "cub", "ecu", "sal", "gua",
+                     "hai", "hon", "mex", "nic", "pan", "par", "per", "dr", "uru" ) #, "ven"
+
+
+# LOOP 1 START ----
+# simple extraction --------------------------------------------------------------------------------
+for( i in 1:length( country_prefix ) ) {
+  
+  message( "\t", "COUNTRY: ", country_prefix[i] )
+  message( "\t", "\t", "Reading tables from pdf file" )
+  
+  # something happens here
+   
+  message("\t", "\t", "\t", "\t", "finished first read")
+  
+  # from this point, a better example is needed. Read manual for eval parse
+  # LOOP 2 START ----
+  for( j in 1:length( get(page_number_vector) ) ) { 
+    
+    table_name <- paste0( country_prefix[i], "_", years[j] )
+    
+    message( "\t", "\t", "\t", "Converting table on page ", 
+             get(page_number_vector)[j], " to data.table: ", table_name )
+    
+    # in this part the code is written but NOT evaluated yet .......................................
+    expr <- expression(
+      {
+        dummy_table_name <- as.data.table( first_read_table_list[j] )
+      }
+    )#expression
+    
+expr <- deparse( expr ) # creates a character vector that contains everything written above
+  expr <- gsub( pattern = '(dummy_table_name)', replacement = table_name,  x = expr ) # insert actual table name
+  eval( eval( parse( text = expr ) ) ) # here the expression is actually evaluated
+      
+  }#loop 2 end
+  
+}#loop 1 end
